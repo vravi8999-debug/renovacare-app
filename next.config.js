83b0dd2775ee3abb03e976912714a1028a8fd13c
vravi8999-debug <vravi8999@gmail.com>
2025-09-28 08:25:31 +0000

    fix: clean next.config.js (remove conflict markers & PWA)

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
 const nextConfig = {
   reactStrictMode: true,
 };
 module.exports = nextConfig;
-=======
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  // turn PWA off for this environment
-  disable: true,
-});
-
-module.exports = withPWA({
-  reactStrictMode: true,
-});
->>>>>>> cd71fa2 (chore: disable next-pwa for Firebase App Hosting)
